2d31eb7e8450939d5d790fae8d985d98f9045a26
renmin <wrmwindmill@gmail.com>
2019-07-18 16:48:42 +0800

    update dangerfile-ios.js and .travis.yml

--- dangerfile-ios.js
+++ dangerfile-ios.js
@@ -17,46 +17,9 @@
  * under the License.
  */
 import { schedule, danger, fail, warn, message, markdown } from "danger";
-import fs from "fs";
-import path from 'path';
-import GitHubApi from 'github';
-import parseDiff from 'parse-diff';
-import shell from "shelljs";
-
-const type_unknown = 0;
-const type_ios_sdk = 1;
-const type_android_sdk = 2;
-const type_ios_test = 3;
-const type_android_test = 4;
-const type_jsfm = 5;
-const type_jsfm_test = 6;
-const type_ui_test = 8;
-
-const getFileType = file => {
-  if (file.match(/WeexSDK\/Sources\/.+\.(m|h|mm)/)) {
-    return type_ios_sdk;
-  } else if (file.match(/WeexSDKTests\//)) {
-    return type_ios_test;
-  } else if (file.match(/android\/sdk\/src\/test\/.+\.java/)) {
-    return type_android_test;
-  } else if (file.match(/android\/sdk\/src\/main\/java\/.+\.java/)) {
-    return type_android_sdk;
-  } else if (
-    file.match(/html5\/(shared|frameworks|render|runtime|services)\/.+\.js/)
-  ) {
-    return type_jsfm;
-  } else if (file.match(/html5\/test\/.+\.js/)) {
-    return type_jsfm_test;
-  } else if(file.match(/test\/scripts\/.+\.js/) || file.match(/test\/pages\/.+\.vue/)){
-    return type_ui_test
-  }else{
-    return type_unknown
-  }
-}
 
 function checkIosFile(file){
-  var type = getFileType(file);
-  return type == type_ios_sdk || type == type_ios_test || type == type_jsfm;
+  return file.match(/ios\//)?true:false;
 }
 
 var hasIosFile = false;
