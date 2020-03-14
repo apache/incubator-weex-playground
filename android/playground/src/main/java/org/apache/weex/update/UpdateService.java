52756cb4b90f9706b4c5af3e45bacf4bc3821c5f
YorkShen <shenyuancs@gmail.com>
2020-03-14 22:39:34 +0800

    Feature(Android): Upgrade Version of dependency. (#25)
    * Feature(Android): Upgrade Android Gradle Plugin to 3.6.1, support library to 28.0.0
    
    1. Upgrade Android Gradle Plugin to 3.6.1
    2. Upgrade support library to 28.0.0
    3. Add x86_64 as ABI
    4. Remove maven mirror in aliyun.

--- android/playground/src/main/java/org/apache/weex/update/UpdateService.java
+++ android/playground/src/main/java/org/apache/weex/update/UpdateService.java
@@ -26,7 +26,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v4.content.FileProvider;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 import com.taobao.weex.WXEnvironment;
 import com.taobao.weex.WXSDKManager;
